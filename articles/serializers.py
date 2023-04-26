ed3ee3d9e88f85316b1d64dadabec9837ce3420b
dana <bear9473@gmail.com>
2023-04-27 03:14:03 +0900

    feat: likes, comment count & some chore

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
 from articles.models import Article, Comment
 
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    def get_user(self, obj):
+        return obj.user.email  #article의 author의 email값
+
+    class Meta:
+        model = Comment
+        exclude = ['article',]
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['content',]
+
+
 class ArticleSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
+    comments = CommentSerializer(many=True)
+    likes = serializers.StringRelatedField(many=True)
+
     def get_user(self, obj):
         return obj.user.email  #article의 author의 email값
     
@@ -12,33 +32,26 @@ class ArticleSerializer(serializers.ModelSerializer):
 
 class ArticleListSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
 
     def get_user(self, obj):
         return obj.user.email  #article의 author의 email값
 
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
     class Meta:
         model = Article
-        fields = ['pk','title','image','updated_at','user']
+        fields = ['pk','title','image','updated_at','user', "comments_count", "likes_count",]
 
 
 class ArticleCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['title','image','content']
+        fields = ['title','image','content',]
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-
-    def get_user(self, obj):
-        return obj.user.email  #article의 author의 email값
-
-    class Meta:
-        model = Comment
-        fields = '__all__'
-
-
-class CommentCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['content',]
