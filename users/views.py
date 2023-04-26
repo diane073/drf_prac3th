26fe001e7f7a7cc1ee63c51a788336e8ba2ae05e
dana <bear9473@gmail.com>
2023-04-27 03:40:06 +0900

    feat: user following,followers, aricle list, like article 등 추가

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework import permissions
 from rest_framework.response import Response
-from users.serializers import UserSerializer, CustomTokenObtainPairSerializer
+from users.serializers import UserSerializer, CustomTokenObtainPairSerializer, UserProfileSerializer
 from rest_framework.generics import get_object_or_404
 from users.models import User
 
@@ -47,3 +47,10 @@ class FollowView(APIView):
         else:
             user_you.followers.add(request.user) #요청 유저가 없으면 추가
             return Response("follow 완료!", status=status.HTTP_200_OK)
+
+
+class ProfileView(APIView):
+    def get(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
